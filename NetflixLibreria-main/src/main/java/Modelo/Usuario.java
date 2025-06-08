301354239268462a6532d54633eac925126dee59
DanyDguezAza <dani9dguez@gmail.com>
2025-06-08 00:40:07 -0600

    ..

---
+++ NetflixLibreria-main/src/main/java/Modelo/Usuario.java
@@ -0,0 +1,106 @@
+package Modelo;
+
+import java.time.LocalDate;
+import org.bson.types.ObjectId;
+public class Usuario {
+    
+    private ObjectId id;
+    private String nombre;
+    private String apellidoPaterno;
+    private String apellidoMaterno;
+    private LocalDate fechaNacimiento;
+    private String correo;
+    private String nombreUsusario;
+    private String contrasenia;
+
+    public Usuario() {
+
+    }
+
+    public Usuario(String nombre, String apellidoPaterno, String apellidoMaterno,
+                   LocalDate fechaNacimiento, String correo, String nombreUsusario, String contrasenia) {
+        this.nombre = nombre;
+        this.apellidoPaterno = apellidoPaterno;
+        this.apellidoMaterno = apellidoMaterno;
+        this.fechaNacimiento = fechaNacimiento;
+        this.correo = correo;
+        this.nombreUsusario = nombreUsusario;
+        this.contrasenia = contrasenia;
+    }
+    
+        public ObjectId getId() {
+        return id;
+    }
+
+    public void setId(ObjectId id) {
+        this.id = id;
+    }
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidoPaterno() {
+        return apellidoPaterno;
+    }
+
+    public void setApellidoPaterno(String apellidoPaterno) {
+        this.apellidoPaterno = apellidoPaterno;
+    }
+
+    public String getApellidoMaterno() {
+        return apellidoMaterno;
+    }
+
+    public void setApellidoMaterno(String apellidoMaterno) {
+        this.apellidoMaterno = apellidoMaterno;
+    }
+
+    public LocalDate getFechaNacimiento() {
+        return fechaNacimiento;
+    }
+
+    public void setFechaNacimiento(LocalDate fechaNacimiento) {
+        this.fechaNacimiento = fechaNacimiento;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getNombreUsusario() {
+        return nombreUsusario;
+    }
+
+    public void setNombreUsusario(String nombreUsusario) {
+        this.nombreUsusario = nombreUsusario;
+    }
+
+    public String getContrasenia() {
+        return contrasenia;
+    }
+
+    public void setContrasenia(String contrasenia) {
+        this.contrasenia = contrasenia;
+    }
+
+    @Override
+    public String toString() {
+        return "Usuario{" +
+                "nombre='" + nombre + '\'' +
+                ", apellidoPaterno='" + apellidoPaterno + '\'' +
+                ", apellidoMaterno='" + apellidoMaterno + '\'' +
+                ", fechaNacimiento=" + fechaNacimiento +
+                ", correo='" + correo + '\'' +
+                ", nombreUsusario='" + nombreUsusario + '\'' +
+                ", contrasenia='" + contrasenia + '\'' +
+                '}';
+    }
+}
